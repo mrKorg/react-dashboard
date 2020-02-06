9fc811cb3b76a4bf0469f299a87095e7144c7c3d
Sergey Gurdzhyian <sergey.gurdzhyian@requestum.com>
2020-02-06 16:09:09 +0200

    Updates

---
+++ precache-manifest.10c84973e82b7054a55a77ebd04277db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f9791298a9abde3c88525195ef563d2",
+    "url": "/react-news-demo/index.html"
+  },
+  {
+    "revision": "44f3d0215e475e53357a",
+    "url": "/react-news-demo/static/css/2.737fc03e.chunk.css"
+  },
+  {
+    "revision": "8387b6a22c7b1c839238",
+    "url": "/react-news-demo/static/css/main.78879b81.chunk.css"
+  },
+  {
+    "revision": "44f3d0215e475e53357a",
+    "url": "/react-news-demo/static/js/2.ffdf8e6e.chunk.js"
+  },
+  {
+    "revision": "9b68f8f16653a7245adec5c616eb6b1f",
+    "url": "/react-news-demo/static/js/2.ffdf8e6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8387b6a22c7b1c839238",
+    "url": "/react-news-demo/static/js/main.fbbb7066.chunk.js"
+  },
+  {
+    "revision": "aa92b0fec6b66ae75eae",
+    "url": "/react-news-demo/static/js/runtime-main.ffffc9c0.js"
+  }
+]);
